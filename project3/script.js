09cacf2cf08e40e17ffea67df3326f9c7445b6f4
ecr74205 <ecr74205@hawkmail.hacc.edu>
2025-11-10 18:35:16 -0500

    Add files via upload

---
+++ project3/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger-menu');
+  const nav = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+});
+
+
+
+
